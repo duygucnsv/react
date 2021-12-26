247248a23c8ab4a1da3536c6fa498eb39e2555dc
duygucnsv <duygucansev@gmail.com>
2021-12-26 16:11:58 +0300

    yorum

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Footer.css";
+function Footer() {
+
+    const yil = new Date().getFullYear();
+
+    return (
+        <footer>
+            <p>Copyright © {yil} </p>
+        </footer>
+
+        )
+}
+
+export default Footer
